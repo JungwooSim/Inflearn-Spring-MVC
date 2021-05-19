4b6f4dd1610853764693323f7e792ef5dea4234d
JungwooSim <cscd053@gmail.com>
2021-05-19 23:52:56 +0900

    7장. 스프링 MVC - 웹 페이지 만들기 / 상품목록 - 타임리프

---
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,36 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 태스트용 데이터 추가
+     * @PostConstruct 어노테이션은 해당 빈의 의존관계가 모두 주입되고 나면 초기화 용도로 호출
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 10));
+    }
+}
